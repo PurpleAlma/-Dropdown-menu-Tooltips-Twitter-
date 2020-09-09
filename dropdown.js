217c961b754fec583a9e9008ccb1bb55e3ce6489
PurpleAlma <64203600+PurpleAlma@users.noreply.github.com>
2020-09-09 17:48:21 +0300

    Add files via upload

---
+++ dropdown.js
@@ -0,0 +1,22 @@
+let menu_items=document.querySelectorAll('.main__menu__item')
+let submenu=document.querySelector('.main-submenu')
+
+menu_items.forEach(menu=>{
+    menu.addEventListener('mouseenter',showSubmenu)
+})
+
+submenu.addEventListener('mouseleave',hideSubmenu)
+
+function showSubmenu(){
+    const submenu=document.querySelector('.main-submenu')
+    submenu.style.display = "flex";
+}
+function hideSubmenu() {
+    const submenu=document.querySelector('.main-submenu')
+    submenu.style.display = "none";
+  }
+
+
+
+let dropdown1= document.querySelector('.main2__menu__item__dropdown1')
+let menu2_item_one=document.querySelector('.main2-menu-item--1')
